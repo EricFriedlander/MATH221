3fa9c1c797a58a2086be755767f2e25d5210b2f9
Eric Friedlander <frieeb@GMS2108-FRIEEB.local>
2022-01-14 21:33:55 -0500

    Added Bush-Gore election.

--- Scripts/gss2018.R
+++ Scripts/gss2018.R
@@ -1,6 +1,6 @@
-gss2018_all <- gss_get_yr(2018)
+gss2002_all <- gss_get_yr(2002)
 
-gss2018 <- gss2018_all %>%
+gss2002 <- gss2002_all %>%
   mutate(
     race = as_factor(racecen1),
     age = cut(age,
@@ -9,9 +9,9 @@ gss2018 <- gss2018_all %>%
               include.lowest=TRUE,),
     degree = as_factor(degree),
     region = as_factor(region)) %>%
-  select(c(age, race, degree, pres16)) %>%
-  filter(pres16 == 1 | pres16 == 2 | pres16 == 3) %>%
-  mutate(pres16 = as_factor(pres16),
+  select(c(age, race, degree, pres00)) %>%
+  filter(pres00 == 1 | pres00== 2 | pres00 == 3) %>%
+  mutate(pres00 = as_factor(pres00),
          race = fct_collapse(race,
                              "asian or pacific islander" = c("asian indian",
                                                              "chinese",
@@ -19,15 +19,18 @@ gss2018 <- gss2018_all %>%
                                                              "japanese",
                                                              "korean",
                                                              "other asian",
-                                                             "guamanian or chamorro"),
+                                                             "guamanian or chamorro",
+                                                             "vietnamese",
+                                                             "samoan",
+                                                             "other pacific islander"),
                              "other" = c("some other race", "NA", "DK")
                              )
   ) %>%
   drop_na()
 
-gss2018 <-slice_sample(gss2018, n=100000, replace=TRUE)
-cell <- c("AT&T", "Verizon", "T-Mobile", "U.S. Cellular", "Dish Wireless")
-gss2018$provider <- cell[sample(1:5, size=100000, replace=TRUE)]
+gss2002 <-slice_sample(gss2002, n=100000, replace=TRUE)
+cell <- c("AT&T", "Verizon", "Sprint", "Comcast", "Time Warner")
+gss2002$provider <- cell[sample(1:5, size=100000, replace=TRUE)]
 
-write_csv(gss2018, file="Datasets/gss2018.csv")
+write_csv(gss2002, file="Datasets/gss2012.csv")
 
