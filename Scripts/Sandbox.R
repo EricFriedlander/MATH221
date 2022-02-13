bc9f15b0063ce468b4e3107b432ad603303b14a0
Eric Friedlander <frieeb@GMS2108-FRIEEB.local>
2022-02-13 16:52:47 -0600

    Added lesson 8.

--- Scripts/Sandbox.R
+++ Scripts/Sandbox.R
@@ -1,71 +1,90 @@
 library(tidyverse)
 library(openintro)
+library(tidymodels)
+library(leaps)
+library(palmerpenguins)
 
-ggplot(data = corr_match, aes(x = x, y = y1)) +
+glimpse(births14)
+
+?births14
+
+births14_clean <- births14 %>%
+  drop_na() %>%
+  select(-lowbirthweight)
+
+births14_clean %>% count(habit)
+
+ggplot(births14_clean, aes(x=habit, y=weight)) +
+  geom_bar(stat="summary", fun="mean")
+
+births14_clean %>% 
+  group_by(habit) %>%
+  summarize(mean(weight))
+
+
+habit_mod <- lm(weight ~ factor(habit), data = births14_clean)
+
+summary(habit_mod)
+
+
+ggplot(births14_clean, aes(x=weeks, y=weight, color=habit)) +
   geom_point()
 
-ggplot(data = corr_match, aes(x = x, y = y2)) +
-  geom_point()
 
-ggplot(data = corr_match, aes(x = x, y = y3)) +
-  geom_point()
+births_mod <- lm(weight ~ habit + weeks, data = births14_clean)
 
-ggplot(data = corr_match, aes(x = x, y = y4)) +
-  geom_point()
+summary(birth_mod)
 
-ggplot(data = corr_match, aes(x = x, y = y5)) +
-  geom_point()
+augmented_mod <- augment(births_mod)
 
-ggplot(data = corr_match, aes(x = x, y = y6)) +
-  geom_point()
-
-ggplot(data = corr_match, aes(x = x, y = y7)) +
-  geom_point()
-
-ggplot(data = corr_match, aes(x = x, y = y8)) +
-  geom_point()
-
-corr_match %>%
-  summarize(cor(x, y1))
-
-corr_match %>%
-  summarize(cor(x, y2))
-
-corr_match %>%
-  summarize(cor(x, y3))
-
-corr_match %>%
-  summarize(cor(x, y4))
-
-corr_match %>%
-  summarize(cor(x, y5))
-
-corr_match %>%
-  summarize(cor(x, y6))
-
-corr_match %>%
-  summarize(cor(x, y7))
-
-corr_match %>%
-  summarize(cor(x, y8))
-
-ggplot(bdims, aes(x=hgt, y=che_di)) +
-  geom_point()
-
-m <- .5
-b <- -57
-
-
-
-ggplot(bdims, aes(x=hgt, y=che_di)) +
+ggplot(augmented_mod, aes(x=weeks, y=weight, color=habit)) +
   geom_point() +
-  geom_abline(intercept = b, slope = m, color="red")
+  geom_line(aes(y = .fitted))
 
-bdims <- bdims %>%
-  mutate(
-    y_pred = m * hgt + b,
-    residuals = che_di - y_pred
-  )
 
-ggplot(bdims, aes(x=hgt, y=residuals)) + 
-  geom_point()
+set.seed(1988)
+#Create training set
+train <- births14_clean %>% sample_frac(.80)
+#Create test set
+test  <- anti_join(births14_clean, train)
+
+train %>% count(habit)
+test %>% count(habit)
+
+# Fit model
+births_mod <- lm(weight ~ habit + weeks + mature + mage, data = train)
+
+# Predict test
+test <- test %>%
+  mutate(predictions = predict(births_mod, newdata=test))
+
+# Compute sum of squared residuals
+test %>%
+  summarize(sse = sum((predictions - weight)^2))
+
+# Summary
+summary(births_mod)
+
+
+# Fit model
+births_mod <- lm(weight ~ fage+mage+mature+weeks+premie+visits+gained+sex+habit+marital+whitemom, data = births14_clean)
+
+# Summary
+summary(births_mod)
+
+births_mod_full <- lm(weight ~ ., data = births14_clean)
+b_mod1 <- stepAIC(births_mod_full)
+
+
+backward <- regsubsets(weight ~ ., data=births14_clean, method = "backward", nvmax=12)
+with(summary(backward), data.frame(adjr2, bic, cp, outmat))
+
+
+forward <- regsubsets(weight ~ ., data=births14_clean, method = "forward", nvmax=12)
+with(summary(forward), data.frame(adjr2, bic, cp, outmat))
+
+
+backward <- regsubsets(weight ~ ., data=births14_clean, method = "exhaustive", nvmax=12)
+with(summary(backward), data.frame(adjr2, bic, cp, outmat))
+
+glimpse(penguins)
